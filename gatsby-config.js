484f56ff774ad37929f1a5fd22fc5f3afd417a7c
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-06-01 11:22:51 -0700

    Add Prismic Integration and basic home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,24 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `team8site`,
+        accessToken: `${process.env.API_KEY}`,
+        schemas: {
+          competition: require("./src/schemas/competition.json"),
+        },
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,58 @@
 import React from "react"
+import { graphql } from "gatsby"
+import "../css/global.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export const pageQuery = graphql`
+  query CompetitionQuery {
+    competition: prismicCompetition {
+      data {
+        title {
+          html
+        }
+        description {
+          html
+        }
+        hero {
+          fixed {
+            src
+          }
+        }
+      }
+    }
+  }
+`
+
+const Index = ({ data: { competition } }) => {
+  const { title, description } = competition.data
+  const image = competition.data.hero.fixed.src
+  return (
+    <React.Fragment>
+      <div>{competition.data.uid}</div>
+      <div dangerouslySetInnerHTML={{ __html: title.html }} />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: description.html,
+        }}
+      />
+      <img src={image} alt="Dummy" />
+    </React.Fragment>
+  )
 }
+
+export default Index
+
+const french = graphql`
+  query filterByLang {
+    allPrismicCompetition(filter: { lang: { eq: "fr-fr" } }) {
+      edges {
+        node {
+          data {
+            title {
+              html
+            }
+          }
+        }
+      }
+    }
+  }
+`
